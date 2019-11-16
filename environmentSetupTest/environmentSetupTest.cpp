165c77adbca532a6dcaabe88d96775fab94db629
clinton <clinton.vanry@gmail.com>
2019-11-16 17:55:47 +0000

    cleanup and use of const

--- environmentSetupTest/environmentSetupTest.cpp
+++ environmentSetupTest/environmentSetupTest.cpp
@@ -1,8 +1,3 @@
-// environmentSetupTest.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-
-
 // https://docs.opencv.org/4.1.2/d3/d52/tutorial_windows_install.html#tutorial_windows_install_path
 // https://docs.opencv.org/4.1.2/dd/d6e/tutorial_windows_visual_studio_opencv.html
 // setx -m OPENCV_DIR D:\OpenCV\Build\x64\vc15
@@ -21,9 +16,8 @@ using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
-	auto filePath =  R"(../environmentSetupTest/clock.jpg)"s;
-	auto img = imread(filePath, IMREAD_COLOR);
+    const auto filePath =  R"(../environmentSetupTest/clock.jpg)"s;
+    const auto img = imread(filePath, IMREAD_COLOR);
 
 	if(img.empty())
 	{
@@ -37,14 +31,3 @@ int main()
 	return 0;
 	
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
